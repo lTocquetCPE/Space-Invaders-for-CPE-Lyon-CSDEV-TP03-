af7541b00ae5812ea8248d824032e1baa6cd3a2e
Alice Malosse <alice.malosse@cpe.fr>
2020-12-18 09:49:40 +0100

    initial game state

--- classes/GameState.py
+++ classes/GameState.py
@@ -5,3 +5,12 @@ class containing all the game variables necessary for the game to work
 
 17/12/20 by Loïc (Pyrrha) TOCQUET and MALOSSE Alice
 """
+
+class GameState() :
+    #init game variable
+    def __init__(self):
+
+        self.gameState="off"
+        self.nbLives=3
+        self.score=0
+        self.listAlien=[]
