207cfb937e57caf588abedfb224398b8615dcfe8
PyrrhaDev <loic.tocquet@gmail.com>
2020-12-18 09:25:53 +0100

    started to work on GameCanvas

--- classes/GameCanvas.py
+++ classes/GameCanvas.py
@@ -5,3 +5,22 @@ class describing the elements on the game canvas. There are two "screens" the ga
 
 17/12/20 by Loïc (Pyrrha) TOCQUET and MALOSSE Alice
 """
+
+from tkinter import Canvas
+
+
+#Contains all the elements necessary for display
+class GameCanvas(Canvas):
+    def __init__(self):
+
+        self.height = 960
+        self.width = 720
+        Canvas.__init__(self, width = self.width, height = self.height)
+
+        self.drawableList = []
+        
+    
+    #TODO updates everything on the canvas (calls a special function in each element)
+    def updateCanvas(self):
+        
+
