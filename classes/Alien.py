6b7356577066ca7d11abf603173521ea564b20a6
Alice Malosse <alice.malosse@cpe.fr>
2021-01-08 09:36:22 +0100

    alien spritesheet

--- classes/Alien.py
+++ classes/Alien.py
@@ -11,23 +11,20 @@ from classes.AnimatedSprite import AnimatedSprite
 
 class Alien () :
     def __init__(self, Alientype):
-        self.type = Alientype
+        self.Alientype = Alientype
         self.speed = 0
 
         #TO DO set all attribut
-        if self.type == "Squid" : #Small Invader
+        if self.Alientype == "Squid" : #Small Invader
             #TO DO reset next attribut
-            self.pos = [0,0]                #position on canvas
-            self.spriteSheetPos = [0,0]     # alien image on sprite sheet
-            self.spriteSize =[50,50]        #size of image we want
-        elif self.type == "Crab" : #Medium Invader
-            self.pos = [0,0]
-            self.spriteSheetPos = [0,0]
-            self.spriteSize = [50,50] 
+            self.pos = [50,50]                #position on canvas
+            self.SpriteSheet = AnimatedSprite("./ressources/SpriteSheet.png", self.pos, [0,0],[11,8],5)
+        elif self.Alientype == "Crab" : #Medium Invader
+            self.pos = [50,100]
+            self.SpriteSheet = AnimatedSprite("./ressources/SpriteSheet.png", self.pos, [12,0],[13,8],5)
         else : #type=="Octopus"   Large Invader
-            self.pos = [0,0]
-            self.spriteSheetPos = [0,0]
-            self.spriteSize = [50,50] 
+            self.pos = [50,200]
+            self.SpriteSheet = AnimatedSprite("./ressources/SpriteSheet.png", self.pos, [25,0],[14,8],5)
 
     def setSprite (self):
         #createSprite=AnimatedSprite()
@@ -35,3 +32,6 @@ class Alien () :
         #createSprite.setSpriteSize(size, [10,10])
         #self.sprite = createSprite.tempImg
         return 0 #juste parce que tout est commentççà
+
+    def draw (self, gameCanvas):
+        self.SpriteSheet.draw(gameCanvas)
--- classes/GameState.py
+++ classes/GameState.py
@@ -6,6 +6,7 @@ class containing all the game variables necessary for the game to work
 17/12/20 by Loïc (Pyrrha) TOCQUET and MALOSSE Alice
 """
 from classes.Canon import Canon
+from classes.Alien import Alien
 
 class GameState() :
     #init game variable
@@ -14,5 +15,6 @@ class GameState() :
         self.gameState="off"
         self.nbLives=3
         self.score=0
-        self.listAlien=[]
         self.canon = Canon()
+        self.listAlien=[Alien("Squid"),Alien("Crab"),Alien("Octopus")]
+        
